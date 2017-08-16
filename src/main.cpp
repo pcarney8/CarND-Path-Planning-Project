57897ad11a10d7a929a14b2607dbe2f27cd4d2f1
Patrick Carney <patrick.s.carney@gmail.com>
2017-08-15 22:08:25 -0400

    ignore build folder. add spline.h and working implementation of a spline

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -196,7 +197,13 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  //Start in lane 1 before a message comes in from the simulator
+  int lane = 1;
+
+  //Reference velocity that we want the car to go
+  double ref_vel = 49.5;
+
+  h.onMessage([&lane,&ref_vel,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -232,14 +239,126 @@ int main() {
 
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
+		
+		//size of the previous path to help smoothly transition to next set of points
+          	int prev_size = previous_path_x.size();
+	
+		//Finite State Machine
 
-          	json msgJson;
+		//Spline Interpolation to smooth paths
+		
+		//vectors of waypoints to include some widely spaced for spline interpolation
+		vector<double> ptsx;
+		vector<double> ptsy;
 
+		//reference x, y, yaw states
+		double ref_x = car_x;
+		double ref_y = car_y;
+		double ref_yaw = deg2rad(car_yaw);
+
+		//if previous size is small use the car as starting reference
+		if (prev_size < 2){
+			double prev_car_x = car_x - cos(car_yaw);
+			double prev_car_y = car_y - sin(car_yaw);
+		
+			ptsx.push_back(prev_car_x);
+			ptsx.push_back(car_x);
+			
+			ptsy.push_back(prev_car_y);
+			ptsy.push_back(car_y);
+			
+		}else{
+		// using previous path's endpoint as a starting reference
+			ref_x = previous_path_x[prev_size - 1];
+			ref_y = previous_path_y[prev_size - 1];
+			
+			double ref_x_prev = previous_path_x[prev_size - 2];
+			double ref_y_prev = previous_path_y[prev_size - 2];
+			
+			ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+			ptsx.push_back(ref_x_prev);
+			ptsx.push_back(ref_x);
+			
+			ptsy.push_back(ref_y_prev);
+			ptsy.push_back(ref_y);
+
+		}
+		
+		//add three evenly spaced 30m points ahead in frenet
+		vector<double> next_wp0 = getXY( car_s + 30, (2 + 4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+		vector<double> next_wp1 = getXY( car_s + 60, (2 + 4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+		vector<double> next_wp2 = getXY( car_s + 90, (2 + 4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+		ptsx.push_back(next_wp0[0]);
+		ptsx.push_back(next_wp1[0]);
+		ptsx.push_back(next_wp2[0]);
+
+		ptsy.push_back(next_wp0[1]);
+		ptsy.push_back(next_wp1[1]);
+		ptsy.push_back(next_wp2[1]);
+
+
+		//transform to local cars coordinates
+		for (int i = 0; i < ptsx.size(); i++) {
+			double shift_x = ptsx[i] - ref_x;
+			double shift_y = ptsy[i] - ref_y;
+
+			ptsx[i] = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));	
+			ptsy[i] = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));	
+		}
+		
+		//initialize spline class and set points to interpolate
+		tk::spline s;
+		s.set_points(ptsx, ptsy);
+
+		//create our vectors for the actual next points		
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
+		//Put all previous path values onto our next values
+		for (int i = 0; i < prev_size; i++){
+			next_x_vals.push_back(previous_path_x[i]);
+			next_y_vals.push_back(previous_path_y[i]);
+		}
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+		//calculate how to break up the spline points so we can travel at reference velocity
+		double target_x = 30.0;
+		double target_y = s(target_x);
+		double target_dist = sqrt( (target_x*target_x) + (target_y*target_y));
+
+		double x_add_on = 0;
+
+		//add on additional point from the spline
+		for (int i = 1; i <= 50-prev_size; i++){
+			//solve for how many N pieces we have
+			double N = (target_dist / (.02 * ref_vel / 2.24));
+			//calculate the x coordinate
+			double x_point = x_add_on + (target_x / N);
+			//let the spline calculate the y
+			double y_point = s(x_point);
+			
+			//move to the next x point
+			x_add_on = x_point;
+			
+			//rotate back to global coordinates
+			double x_ref = x_point;
+			double y_ref = y_point;
+			
+			x_point = (x_ref * cos(ref_yaw)) - (y_ref * sin(ref_yaw));
+			y_point = (x_ref * sin(ref_yaw)) + (y_ref * cos(ref_yaw));
+		
+			x_point += ref_x;
+			y_point += ref_y;
+
+			next_x_vals.push_back(x_point);
+			next_y_vals.push_back(y_point);
+
+		}
+
+
+		//put values back in json and send back to simulator
+		json msgJson;
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
